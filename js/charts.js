19f9959e1308c5bd05c7d7ce5f4c76e9257ca81c
VieVie31 <orissermaroix@gmail.com>
2017-05-10 18:00:27 +0200

    fixing a graph refreshing problem...

--- js/charts.js
+++ js/charts.js
@@ -1,60 +1,58 @@
 function refresh(value) {
-        var f1 = document.getElementById("field1");
-        var f2 = document.getElementById("field2");
-        var f1Val = f1.options[f1.selectedIndex].value;
-        var f2Val = f2.options[f2.selectedIndex].value;
+  var f1 = document.getElementById("field1");
+  var f2 = document.getElementById("field2");
+  var f1Val = f1.options[f1.selectedIndex].value;
+  var f2Val = f2.options[f2.selectedIndex].value;
 
-        //does not have the same value
-        if (f1Val==f2Val) {return;}
-        
-        var stats_on_visible_marker = getMyStats(f1Val,f2Val);
-        console.log(stats_on_visible_marker);
-        drawChart(stats_on_visible_marker,f1Val,f2Val);
+  //does not have the same value
+  if (f1Val==f2Val) {return;}
+  
+  var stats_on_visible_marker = getMyStats(f1Val,f2Val);
+  console.log(stats_on_visible_marker);
+  drawChart(stats_on_visible_marker,f1Val,f2Val);
+}
+
+function getCount(attName, countAttName, markers) {
+  var ht = [];
+
+  for (var i = 0; i < markers.length; i++) {
+    var t = parseInt(markers[i][countAttName]);
+    var ht_val = ht[markers[i][attName]];
+    ht_val = ((ht_val == '' + NaN) || (ht_val == null) ? 0 : ht_val);
+    ht[markers[i][attName]] = ht_val + (t == '' + NaN ? 0 : t);
+  }
+
+  var k = [];
+  for(var i=0; i<markers.length; i++) {
+    if (k.indexOf(markers[i][attName]) == -1) {
+      k.push(markers[i][attName]);
     }
+  }
 
-    function getCount(attName, countAttName, markers) {
-      /*attName = "day";
-      countAttName = "nb_killed";*/
+  out = [[attName,countAttName]];
 
-        ht = [];
-        for (var i = 0; i < markers.length; i++) {
-          var t = parseInt(markers[i][countAttName]);
-          var ht_val = ht[markers[i][attName]];
-          ht_val = ((ht_val == '' + NaN) || (ht_val == null) ? 0 : ht_val);
-          ht[markers[i][attName]] = ht_val + (t == '' + NaN ? 0 : t);
-        }
-        var k = [];
-        for(var i=0; i<markers.length; i++) {
-          if (k.indexOf(markers[i][attName]) == -1) {
-            k.push(markers[i][attName]);
-          }
-        }
+  for (var i = 0; i<k.length; i++) {
+    out.push([k[i],ht[k[i]]]);
+  }
+  
+  return out;
+}
 
-        out = [[attName,countAttName]];
-        //var k = Object.keys(ht);
-        //console.log(k);
-
-        for (var i = 0; i<k.length; i++) {
-            out.push([k[i],ht[k[i]]]);
-          }
-        console.log(out);
-        return out;
-    }
-
-    function getMyStats(f1,f2) {
-        return getCount(f1, f2, get_visible_markers());
-    }
+function getMyStats(f1,f2) {
+  return getCount(f1, f2, get_visible_markers());
+}
 
 function drawChart(array,f1,f2) {
-
   var data = google.visualization.arrayToDataTable(array);
 
   var options = {
     title: f1 + ' by nb ' + f2,
     pieSliceText: 'label',
-    sliceVisibilityThreshold: .01
-    };
-    var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+    sliceVisibilityThreshold: .01,
+    height: parseInt($("#div2").css("height")) - 50,
+    width: parseInt($("#div2").css("width"))
+  };
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
-    chart.draw(data, options);
+  chart.draw(data, options);
 }
